7c671480738d243f69c23162241df1c4009b63b4
Emil Stenberg <esandin@gmail.com>
2015-12-08 11:00:10 +0100

    added demo for safari

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
 function deviceOrientationListener(event) {
-	System.import('index.js').then(heading => {
-		const _heading = heading.compassHeading({
+	System.import('index.js').then(function(heading){
+		// safari works differently, but has a event.webkitCompassHeading
+		console.log('imported heading')
+		const _heading = event.compassHeading || event.webkitCompassHeading || heading.compassHeading({
 			gamma: event.gamma,
 			beta: event.beta,
 			alpha: event.alpha
